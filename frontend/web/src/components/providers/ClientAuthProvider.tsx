16199be5cd96a5047256ae229da839fe56c380f0
Ryoji Fukaya <fukaya55822002@gmail.com>
2025-09-01 16:10:47 +0900

    Remove web submodule completely

--- frontend/web/src/components/providers/ClientAuthProvider.tsx
+++
@@ -1,8 +0,0 @@
-// src/components/providers/ClientAuthProvider.tsx
-'use client';
-
-import { AuthProvider } from '@/hooks/useAuth';
-
-export function ClientAuthProvider({ children }: { children: React.ReactNode }) {
-  return <AuthProvider>{children}</AuthProvider>;
-}
